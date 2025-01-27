78ce341053ce022fa506e090cbb2c42624cb9e7b
wesley <wesley.rochao@hotmail.com>
2025-01-27 14:49:09 -0300

    Initial commit

---
+++ laboratorio/Echo.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace laboratorio
+{
+    public class Echo
+    {
+        private readonly ILogger<Echo> _logger;
+
+        public Echo(ILogger<Echo> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("Echo")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            using (StreamReader reader = new StreamReader(req.Body))
+            {
+                string name = await reader.ReadToEndAsync();
+                await response.WriteStringAsync($"Hello, {name}");
+            }
+
+            return response;
+        }
+    }
+}
---
+++ laboratorio/GetSettingInfo.cs
@@ -0,0 +1,31 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace laboratorio
+{
+    public class GetSettingInfo
+    {
+        private readonly ILogger<GetSettingInfo> _logger;
+
+        public GetSettingInfo(ILogger<GetSettingInfo> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetSettingInfo")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [BlobInput("content/settings.json", Connection = "AzureWebJobsStorage")] string blobContent)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation($"Blob content: {blobContent}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString($"blobContent");
+
+            return await response;
+        }
+    }
+}
